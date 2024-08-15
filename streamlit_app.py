bed773a50bf23b07114eaa9ca2490e0d4499f4c0
parker84 <brydonparker4@gmail.com>
2024-08-15 09:09:54 -0400

    adding tests adding 365d going back 5 years removing unused comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.logo("assets/TorCrime_logo.png")
 
 # --------------constants
 DEFAULT_STOCK_SYMBOLS = ["NVDA", "META", "AMZN", "MSFT", "SHOP", "TSLA", "AAPL"]
-DAYS_BACK = [1, 7, 30,90, 180] # TODO: 365d change doesn't work
+DAYS_BACK = [1, 7, 30,90, 180, 365]
 CHANGE_ICONS = {
     'Big Negative Drop < -10%': '📉',
     'Small Negative Drop [-1%, 10%]': '🔻',
@@ -41,13 +41,26 @@ def get_data_for_stocks(stock_symbols, start_date, end_date):
     logger.info(f'🏃 Fetching data for {stock_symbols} - {start_date} to {end_date}')
     stock_data = yf.download(stock_symbols, start=start_date, end=end_date)
     logger.info(f'✅ Fetched data for {stock_symbols} - {start_date} to {end_date}')
+    stock_data.sort_values(by=['Date'], inplace=True)
     return stock_data
 
+def get_pct_changes(stock_data, days_back):
+    pct_changes = {}
+    for days_back in DAYS_BACK:
+        pct_changes[days_back] = stock_data['Adj Close'].pct_change(days_back, fill_method=None)
+    return pct_changes
+
+def test_pct_changes(stock_data, pct_changes, days_back):
+    for days_back in DAYS_BACK:
+        expected_pct_change = (stock_data['Adj Close'].iloc[-1] / stock_data['Adj Close'].iloc[-(days_back+1)]) - 1
+        actual_pct_change = pct_changes[days_back].iloc[-1]
+        equals = expected_pct_change == actual_pct_change
+        assert int(equals.sum()) == len(equals), f"Expected {expected_pct_change}, got {actual_pct_change}"
 
 # --------------parameters
 tickers = get_sp500_tickers(todays_date) + ['SHOP', 'AAPL']
 
-start_date = st.sidebar.date_input("Start Date", value=datetime.now() - timedelta(days=370))
+start_date = st.sidebar.date_input("Start Date", value=datetime.now() - timedelta(days=365*5))
 end_date = st.sidebar.date_input("End Date", value=todays_date)
 st.sidebar.caption("Made with ❤️ by [Brydon](https://brydon.streamlit.app/)")
 
@@ -61,15 +74,15 @@ stock_data = get_data_for_stocks(stock_symbols, start_date, end_date)
 logger.info(f'stock_data_df: {stock_data}')
 logger.info(f'stock_data columns: {stock_data.columns}')
 
-logger.info(f'🏃 Plotting data for {stock_symbols} - {start_date} to {end_date}')
-# st.line_chart(stock_data['Adj Close'])
+logger.info('➗ Getting % Changes')
+pct_changes = get_pct_changes(stock_data, DAYS_BACK)
+logger.info('✅ Got % Changes')
 
-pct_changes = {}
-for days_back in DAYS_BACK:
-    # TODO: 365d change doesn't work
-    pct_changes[days_back] = stock_data['Adj Close'].pct_change(days_back, fill_method=None)
-# TODO: bring this into the dataframe
+logger.info('🧪 Testing % Changes')
+test_pct_changes(stock_data, pct_changes, DAYS_BACK)
+logger.info('✅ % Change Tests Passed')
 
+logger.info('⚙️ Processing Data')
 df_viz = stock_data['Adj Close'].iloc[[-1]].T
 df_viz.columns = ['Adjusted Close Price']
 
@@ -92,21 +105,13 @@ for days_back in DAYS_BACK:
             else:
                 color_coded.append('')
     df_viz[f'pct_change_{days_back}d'] = [f"{val:.1f}% {emoji}" for val, emoji in zip(df_viz[f'pct_change_{days_back}d'], color_coded)]
+logger.info('✅ Data Processed')
+
+logger.info('📊 Visualizing Data')
 
 pct_change_col_configs = {
-    # f'pct_change_{days}d': st.column_config.ProgressColumn(
-    #     f"% Change ({days}d)",
-    #     format="%.1f%%",
-    #     width="medium",
-    #     min_value=df_viz[f'pct_change_{days}d'].min(),
-    #     max_value=df_viz[f'pct_change_{days}d'].max(),
-    # )
     f'pct_change_{days}d': st.column_config.TextColumn(
         f"% Change ({days}d)",
-        # format="%.1f%%",
-        # width="medium",
-        # min_value=df_viz[f'pct_change_{days}d'].min(),
-        # max_value=df_viz[f'pct_change_{days}d'].max(),
     )
    for days in DAYS_BACK
 }
@@ -125,21 +130,6 @@ with col2:
         st.caption(f"{emoji} = {change}")
 
 
-
-# TODO: line chart for adj close (maybe market cap)
-# TODO: better line chart for adj close
-# TODO: pie chart for market cap
-
-# TODO: include big metrics at the top + changes (every 30d?)
-
-# TODO: email screenshots every week?
-
-
-# TODO: table with change in last 30/365/7/1 days
-# TODO: colour code the changes with emojis
-# TODO: include the line charts in the table?
-
-
 col1, col2 = st.columns(2)
 with col1:
     p = px.line(
@@ -147,10 +137,6 @@ with col1:
         title=f"Daily Adjusted Close Prices",
         labels={"value": "Price", "Date": "Date"},
     )
-    # if stock_symbols == DEFAULT_STOCK_SYMBOLS:
-    #     for symbol in DEFAULT_STOCK_SYMBOLS:
-    #         if symbol not in ['AAPL', 'AMZN']:
-    #             p.update_traces(visible="legendonly", selector=dict(name=symbol))
     st.plotly_chart(p, use_container_width=True)
 
 with col2:
@@ -160,3 +146,7 @@ with col2:
         labels={"value": "Volume", "Date": "Date"},
     )
     st.plotly_chart(p, use_container_width=True)
+
+logger.info('✅ Data Visualized')
+
+logger.info('🏁 Done')
